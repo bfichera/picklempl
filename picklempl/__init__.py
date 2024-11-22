2ffa2e262bd1b61818f329cfb3cd9540e2b25189
bryan thomas fichera <bfichera@sdflogin003.sdf.slac.stanford.edu>
2024-11-22 10:57:48 -0800

    Initial commit

---
+++ picklempl/__init__.py
@@ -0,0 +1,20 @@
+import pickle
+
+from matplotlib import pyplot as plt
+
+def dump(filename):
+    fig = plt.gcf()
+    with open(filename, 'wb') as fh:
+        pickle.dump(fig, fh)
+
+
+def plot(filename):
+    with open(filename, 'rb') as fh:
+        fig = pickle.load(fh)
+    plt.show()
+
+
+def load(filename):
+    with open(filename, 'rb') as fh:
+        fig = pickle.load(fh)
+    return fig
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(
+    name='picklempl',
+    packages=['picklempl'],
+)
